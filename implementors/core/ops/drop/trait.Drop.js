76c9f34fe52d03035c58069ce3cc6642d8716495
Travis CI User <travis@example.org>
2020-06-16 10:57:38 +0000

    Update documentation

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["parity_tokio_ipc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"parity_tokio_ipc/struct.Endpoint.html\" title=\"struct parity_tokio_ipc::Endpoint\">Endpoint</a>","synthetic":false,"types":["parity_tokio_ipc::unix::Endpoint"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/tokio/io/async_read/trait.AsyncRead.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["parity_tokio_ipc"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/tokio/0.2.21/tokio/io/async_read/trait.AsyncRead.html\" title=\"trait tokio::io::async_read::AsyncRead\">AsyncRead</a> for <a class=\"struct\" href=\"parity_tokio_ipc/struct.Connection.html\" title=\"struct parity_tokio_ipc::Connection\">Connection</a>","synthetic":false,"types":["parity_tokio_ipc::unix::Connection"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
